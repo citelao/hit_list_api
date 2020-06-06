4585f0691e7a577e71379bebcf9f3fd9cf63f2b6
Ben <ben@stolovitz.com>
2020-06-06 19:56:08 -0400

    create a library class

--- src/api/Library.ts
+++ src/api/Library.ts
@@ -1,21 +1,114 @@
-// import sqlite3 from "sqlite3";
+import sqlite3 from "sqlite3";
+import SqlString from "sqlstring";
 
-// const PATH = "/Users/citelao/Library/Application Support/The Hit List/The Hit List Library.thllibrary/library.sqlite3";
+export interface ITask {
+    title: string;
+};
 
-// export type ITask {
-//     title: string;
-// };
+export interface ITag {
+    // TODO;
+}
 
-// // export default class Library {
+// TODO cleanup
 
-// //     private readonly db: sqlite3.Database;
+async function all<T>(db: sqlite3.Database, query: string): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
 
-// //     constructor() {
-// //         this.db = new sqlite3.Database(PATH, sqlite3.OPEN_READONLY);
-// //         // TODO path;
-// //     }
+            resolve(rows);
+        });
+    })
+}
 
-// //     public async GetTasks(): Promise<ITask[]> {
+async function get<T>(db: sqlite3.Database, query: string): Promise<T> {
+    return new Promise((resolve, reject) => {
+        db.get(query, (err, row) => {
+            if (err) {
+                reject(err);
+            }
 
-// //     }
-// // }
+            resolve(row);
+        });
+    })
+}
+
+// async function getTasks(count = 20): Promise<ITask[]> {
+//     const statement = SqlString.format(
+//         "SELECT ZTITLE AS title FROM ZTASK LIMIT ?",
+//         [count]
+//     )
+//     return await all<ITask>(db, statement);
+// }
+
+interface IGroup {
+    Z_PK: number;
+    ZTITLE: string;
+}
+
+function getTagGroup(baseGroups: IGroup[]): IGroup {
+    const group = baseGroups.find((group) => group.ZTITLE === "Tags");
+    if (!group) {
+        throw new Error("No tag group!");
+    }
+
+    return group;
+}
+
+export default class Library {
+    private readonly db: sqlite3.Database;
+
+    private rootGroup!: IGroup;
+    private tagGroup!: IGroup;
+
+    public static async create(path: string) {
+        const lib = new Library(path);
+        await lib.initialize();
+        return lib;
+    }
+
+    private constructor(path: string) {
+        this.db = new sqlite3.Database(path, sqlite3.OPEN_READONLY);
+    }
+
+    private async initialize() {
+        this.rootGroup = await this.getRootGroup();
+        const baseGroups = await this.getChildGroups(this.rootGroup.Z_PK);
+        this.tagGroup = getTagGroup(baseGroups);
+    }
+
+    public async getTags(): Promise<ITag[]> {
+        return await this.getChildGroups(this.tagGroup.Z_PK);
+    }
+
+    public close(): void {
+        this.db.close();
+    }
+
+    // Private helpers
+    private async getGroups(count = 20): Promise<any[]> {
+        const statement = SqlString.format(
+            "select * from ZGROUP order by ZDISPLAYORDER limit ?",
+            [count]
+        )
+        return await all<any>(this.db, statement);
+    }
+
+    private async getRootGroup(): Promise<IGroup> {
+        const statement = SqlString.format(
+            "select * from ZGROUP where ZPARENTGROUP is NULL",
+            []
+        );
+        return await get<any>(this.db, statement);
+    }
+
+    private async getChildGroups(groupId: number): Promise<IGroup[]> {
+        const statement = SqlString.format(
+            "select * from ZGROUP where ZPARENTGROUP is ? order by ZDISPLAYORDER",
+            [groupId]
+        );
+        return await all<any>(this.db, statement);
+    }
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,87 +1,11 @@
-import sqlite3 from "sqlite3";
-import SqlString from "sqlstring";
+import Library from "../api/Library";
 
 const PATH = "/Users/citelao/Library/Application Support/The Hit List/The Hit List Library.thllibrary/library.sqlite3";
 
-async function all<T>(db: sqlite3.Database, query: string): Promise<T[]> {
-    return new Promise((resolve, reject) => {
-        db.all(query, (err, rows) => {
-            if (err) {
-                reject(err);
-            }
-
-            resolve(rows);
-        });
-    })
-}
-
-async function get<T>(db: sqlite3.Database, query: string): Promise<T> {
-    return new Promise((resolve, reject) => {
-        db.get(query, (err, row) => {
-            if (err) {
-                reject(err);
-            }
-
-            resolve(row);
-        });
-    })
-}
-
-const db = new sqlite3.Database(PATH, sqlite3.OPEN_READONLY);
-
-interface ITask {
-    title: string;
-}
-
-async function getTasks(count = 20): Promise<ITask[]> {
-    const statement = SqlString.format(
-        "SELECT ZTITLE AS title FROM ZTASK LIMIT ?",
-        [count]
-    )
-    return await all<ITask>(db, statement);
-}
-
-interface IGroup {
-    Z_PK: number;
-    ZTITLE: string;
-}
-
-async function getGroups(count = 20): Promise<any[]> {
-    const statement = SqlString.format(
-        "select * from ZGROUP order by ZDISPLAYORDER limit ?",
-        [count]
-    )
-    return await all<any>(db, statement);
-}
-
-async function getRootGroup(): Promise<IGroup> {
-    const statement = SqlString.format(
-        "select * from ZGROUP where ZPARENTGROUP is NULL",
-        []
-    );
-    return await get<any>(db, statement);
-}
-
-async function getChildGroups(groupId: number): Promise<IGroup[]> {
-    const statement = SqlString.format(
-        "select * from ZGROUP where ZPARENTGROUP is ? order by ZDISPLAYORDER",
-        [groupId]
-    );
-    return await all<any>(db, statement);
-}
-
 ((async () => {
-    // const tasks = await getTasks();
-    // tasks.forEach((t) => console.log(t.title));
-
-    const root = await getRootGroup();
-    const baseGroups = await getChildGroups(root.Z_PK);
-    console.dir(baseGroups);
-
-    // (await getGroups(400)).forEach((v) => console.dir(v));
-
-    // (await all<any>(db, "SELECT * from ZGROUP LIMIT 20")).forEach((t) => console.dir(t));
-    db.close();
+    const library = await Library.create(PATH);
+    console.dir(await library.getTags());
+    library.close();
 })()).catch((reason) => {
     throw reason;
 })
