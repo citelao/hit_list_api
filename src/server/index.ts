3f6c70f2228b4f15142fb4e16c74dc56d4b5e277
Ben Stolovitz <ben@stolovitz.com>
2021-05-06 22:34:47 -0400

    list tree

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import Library from "../api/Library";
 const app = express();
 const port = 3000;
 
 app.set("views", "static/views/");
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render("index", {});
+// TODO: dynamic path.
+const PATH = "/Users/citelao/Library/Application Support/The Hit List/The Hit List Library.thllibrary/library.sqlite3";
+
+app.get('/', async (req, res) => {
+  const library = await Library.create(PATH);
+  const lists = await library.getLists();
+  res.render("index", { lists });
 });
 
 app.listen(port, () => {
