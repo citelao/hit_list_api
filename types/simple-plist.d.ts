04ffcd98b8d900ff89356e24dbfbd193312db945
Ben Stolovitz <ben@stolovitz.com>
2021-05-07 00:20:01 -0400

    types

--- types/simple-plist.d.ts
+++ types/simple-plist.d.ts
@@ -1,4 +1,7 @@
 declare module "simple-plist" {
+    export function readFileSync(path: string): unknown;
+    export function readFile(path: string, callback: (err: unknown, results: unknown) => void): unknown;
+
     export function stringify(obj: Object): string;
 
     export function parse(strOrBuffer: String | Buffer): unknown;
