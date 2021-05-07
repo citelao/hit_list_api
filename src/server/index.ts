7042713a09276127f3abf53df9fc3150437b789a
Ben Stolovitz <ben@stolovitz.com>
2021-05-06 22:42:46 -0400

    list clicks

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import Library from "../api/Library";
+import Library, { IFolder, IList } from "../api/Library";
 const app = express();
 const port = 3000;
 
@@ -9,10 +9,44 @@ app.set("view engine", "ejs");
 // TODO: dynamic path.
 const PATH = "/Users/citelao/Library/Application Support/The Hit List/The Hit List Library.thllibrary/library.sqlite3";
 
+// TODO copied from from cli.
+function findList(lists: Array<IList | IFolder>, predicate: (list: IList | IFolder) => boolean): IList | IFolder | null {
+  for(let i = 0; i < lists.length; i++) {
+      const list = lists[i];
+      if (predicate(list)) {
+          return list;
+      }
+
+      if (list.type === "folder" && list.children) {
+          const child = findList(list.children, predicate);
+          if (child) {
+              return child;
+          }
+      }
+  }
+
+  return null;
+}
+
 app.get('/', async (req, res) => {
   const library = await Library.create(PATH);
   const lists = await library.getLists();
   res.render("index", { lists });
+});
+
+app.get('/list/:listId', async (req, res) => {
+  const library = await Library.create(PATH);
+  const lists = await library.getLists();
+  const listId = parseInt(req.params.listId);
+  const list = findList(lists, (l) => l.id === listId);
+  if (!list) {
+    throw new Error(`Could not find list ${listId}.`);
+  }
+  if (list.type === "folder") {
+      throw new Error(`List ${listId} is a folder`);
+  }
+  const tasks = await library.getTasks(list);
+  res.render("list", { lists, list, tasks });
 });
 
 app.listen(port, () => {
