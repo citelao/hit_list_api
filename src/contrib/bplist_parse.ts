3e7203d34029cd3d7d4bab50e6e1dfff12bedac9
Ben Stolovitz <ben@stolovitz.com>
2021-05-07 00:19:58 -0400

    bplist parse

--- src/contrib/bplist_parse.ts
+++ src/contrib/bplist_parse.ts
@@ -1,14 +1,19 @@
 import plist from "simple-plist";
 
-// Convert a Javascript object to a plist xml string
-var xml = plist.stringify({ name: 'Joe', answer: 42 })
-console.log(xml) // output is a valid plist xml string
+// // Convert a Javascript object to a plist xml string
+// var xml = plist.stringify({ name: 'Joe', answer: 42 })
+// console.log(xml) // output is a valid plist xml string
 
-// Convert a plist xml string or a binary plist buffer to a Javascript object
-var data = plist.parse(
-  '<plist><dict><key>name</key><string>Joe</string></dict></plist>'
-)
-console.log(JSON.stringify(data));
+// // Convert a plist xml string or a binary plist buffer to a Javascript object
+// var data = plist.parse(
+//   '<plist><dict><key>name</key><string>Joe</string></dict></plist>'
+// )
+// console.log(JSON.stringify(data));
 
-// Note this isn't actually a BPLIST but you can just save the blob from the
-// SQLITE as a .plist and quicklook will preview it properly :)
+if (process.argv.length !== 3) {
+    console.log("Pass a file!");
+    process.exit(1);
+}
+
+const data = plist.readFileSync(process.argv[2]);
+console.log(JSON.stringify(data))
