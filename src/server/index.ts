e91390d0f68c3383f3190bc852290f3d3db7adb2
Ben Stolovitz <ben@stolovitz.com>
2021-05-06 22:24:01 -0400

    clean up

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
-import path from "path";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.set("views", "static/views/");
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
   res.render("index", {});
-})
+});
 
 app.listen(port, () => {
-  console.log(`Started webapp at http://localhost:${port}`)
-})
+  console.log(`Started webapp at http://localhost:${port}`);
+});
